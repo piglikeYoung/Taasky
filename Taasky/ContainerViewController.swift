613e75585b721b4d00eb15313128fd25b0faf2ac
piglikeyoung <316046510@qq.com>
2015-09-15 16:40:46 +0800

    完成隐藏，显示菜单栏，修复scrollView.pagingEnabled的bug

--- Taasky/ContainerViewController.swift
+++ Taasky/ContainerViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ContainerViewController: UIViewController {
+class ContainerViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var menuContainerView: UIView!
+    
     private var detailViewController: DetailViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        hideOrShowMenu(false, animated: false)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -27,10 +31,27 @@ class ContainerViewController: UIViewController {
     
     var menuItem: NSDictionary? {
         didSet {
+            hideOrShowMenu(false, animated: true)
             if let detailViewController = detailViewController {
                 detailViewController.menuItem = menuItem
             }
         }
+    }
+    
+    func hideOrShowMenu(show: Bool, animated: Bool) {
+        // menuOffset就是menuContainView的宽度80，如果是true，偏移量就是0，菜单栏不显示
+        let menuOffset = CGRectGetWidth(menuContainerView.bounds)
+        scrollView.setContentOffset(show ? CGPointZero : CGPoint(x: menuOffset, y: 0), animated: animated)
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        /*
+        Fix for the UIScrollView paging-related issue mentioned here:
+        http://stackoverflow.com/questions/4480512/uiscrollview-single-tap-scrolls-it-to-top
+        */
+        // ScrollView的偏移量和菜单栏宽度相等的时候，禁用pagingEnabled
+        scrollView.pagingEnabled = scrollView.contentOffset.x < (scrollView.contentSize.width - CGRectGetWidth(scrollView.frame))
     }
 
 }
---
+++ Taasky/HamburgerView.swift
@@ -0,0 +1,21 @@
+//
+//  HamburgerView.swift
+//  Taasky
+//
+//  Created by piglikeyoung on 15/9/15.
+//  Copyright (c) 2015年 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class HamburgerView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
