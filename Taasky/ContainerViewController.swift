19716393cbd7a022d6c5d51e6ea1c8dabb548495
piglikeyoung <316046510@qq.com>
2015-09-15 14:38:05 +0800

    创建Content View

---
+++ Taasky/ContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContainerViewController.swift
+//  Taasky
+//
+//  Created by piglikeyoung on 15/9/15.
+//  Copyright (c) 2015年 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
